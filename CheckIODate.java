0f31ab7113ff16fee0f68c36915cf42596d8096f
Doctor-23815 <91544694+Doctor-23815@users.noreply.github.com>
2021-11-29 04:07:46 +0000

    Add files via upload

---
+++ CheckIODate.java
@@ -0,0 +1,42 @@
+import java.util.StringTokenizer;
+
+/**
+ * A hotel reservation check in and check out date.
+ */
+
+public class CheckIODate {
+
+    private int year;
+    private int month;
+    private int day;
+
+    public String toString() {
+        String s = this.day + "/" + this.month + "/" + this.year;
+        return s;
+    }
+
+    public void checkInDate(String s) {
+        String[] input = s.split("/");
+        day = Integer.valueOf(input[0]);
+        month = Integer.valueOf(input[1]);
+        year = Integer.valueOf(input[2]);
+    }
+
+    public void checkOutDate(String s) {
+        String[] input = s.split("/");
+        day = Integer.valueOf(input[0]);
+        month = Integer.valueOf(input[1]);
+        year = Integer.valueOf(input[2]);
+    }
+
+    public boolean equals(Object obj) {
+        if(obj==null || !(obj instanceof CheckIODate)){
+            return false;
+        }
+        else if(this==obj){
+            return true;
+        }
+        CheckIODate x = ((CheckIODate)obj);
+        return (x.year==year && x.month==month && x.day==day);
+    }
+}
