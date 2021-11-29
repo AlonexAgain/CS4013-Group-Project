20037269da1de721494254aa47aad434e5887d64
elliotbuckley <91120011+elliotbuckley@users.noreply.github.com>
2021-11-29 18:16:20 +0000

    Add files via upload

--- Reservation.java
+++ Reservation.java
@@ -14,13 +14,14 @@ public class Reservation {
     private CheckIODate date;
     private CheckIOTime from;
     private CheckIOTime till;
+    
 
     public boolean fallsOn(CheckIODate x) {
         return (x.equals(date));
     }
 
     public String format() {
-        String s = "Date: " + date.toString() + "Time From: " + from.toString() + "Time Till: " + till.toString() + "Description: " + description;
+        String s = "Date: " + date.toString() + "\n" + "Time From: " + from.toString() + "\n" + "Time Till: " + till.toString() + "\n" + "Description: " + description;
         return s;
     }
     
@@ -34,6 +35,8 @@ public class Reservation {
         date = new CheckIODate(input[input.length-3]);
     }
     
+    
+    
     public boolean equals(Object obj) {
         if(obj==null || !(obj instanceof Reservation)){
             return false;
@@ -44,4 +47,63 @@ public class Reservation {
         Reservation x = ((Reservation)obj);
         return (x.date.equals(date) && x.from.equals(from) && x.till.equals(till));
     }
+    
+    
+    
+    /*
+     *variables for the customer details     
+     */
+	private String firstName;
+	private String lastName;
+	private String phoneNo;
+	private String email;
+	
+	/*
+	 *Create the set and get operators for each parameter  
+	 */
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/*
+	 * An overrided toString() method to format the customers information0
+	 */
+	
+	@Override
+	public String toString() {
+		return "First Name: " + this.firstName + "\n"
+				+ "Last Name: " + this.lastName + "\n"
+				+ "Phone Number: " + this.phoneNo + "\n"
+				+ "Email: " + this.email + "\n";
+	}
+	
 }
