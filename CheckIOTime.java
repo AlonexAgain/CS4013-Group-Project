8098cb3b33dc85a944d2d315a69784fe394918e0
Doctor-23815 <91544694+Doctor-23815@users.noreply.github.com>
2021-11-29 04:16:30 +0000

    CheckIOTime ver.1

---
+++ CheckIOTime.java
@@ -0,0 +1,40 @@
+import java.util.StringTokenizer;
+
+// This class...
+
+/**
+ * A hotel reservation check in and check out time.
+ */
+
+public class CheckIOTime {
+    private int hours;
+    private int minutes;
+
+    public String toString() {
+        String s = this.hours + ":" + this.minutes;
+        return s;
+    }
+
+    public void CheckInTime(String s) {
+        String[] input = s.split(":");
+        hours = Integer.valueOf(input[0]);
+        minutes = Integer.valueOf(input[1]);
+    }
+
+    public void CheckOutTime(String s) {
+        String[] input = s.split(":");
+        hours = Integer.valueOf(input[0]);
+        minutes = Integer.valueOf(input[1]);
+    }
+
+    public boolean equals(Object obj) {
+        if(obj==null || !(obj instanceof CheckIOTime)){
+            return false;
+        }
+        else if(this==obj){
+            return true;
+        }
+        CheckIOTime x = ((CheckIOTime)obj);
+        return (x.hours==hours && x.minutes==minutes);
+    }
+}
