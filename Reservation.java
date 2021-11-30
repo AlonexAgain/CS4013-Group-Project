7c7647c43ede02f3cea3fa97603cbcfe137a7344
Saw <76816580+AlonexAgain@users.noreply.github.com>
2021-11-30 11:38:35 +0000

    Add files via upload

--- Reservation.java
+++ Reservation.java
@@ -3,41 +3,54 @@ import java.util.Random;
 
 public class Reservation {
 
-	/*
-	 * Variables for the user details
-	 */
-	private String name;
-	private String email;
-	private String phoneNo;
-	
-	
-	
-    /*
-     * Code to allow the user to add their name
-     */
+    private String name;
+    private String email;
+    private String phoneNo;
+
     public void nameUser(){
 
         Scanner scanr = new Scanner(System.in);
 
-        //String name;
+        String name;
 
         System.out.print("Name: ");
         name = scanr.nextLine().toUpperCase();
         this.setName(name);
+
+        System.out.println("                   ");
         System.out.println("Name: " + name);
 
-        scanr.close();
+        
     }
+
     
-    /*
-     * Code to generate a random reservation number
-     */
+    public void emailUser() {
+        Scanner scanr = new Scanner(System.in);
+
+        String email;
+        System.out.print("Email: ");
+        email = scanr.nextLine().toUpperCase();
+        this.setEmail(email);
+        System.out.println("Email: " + email);
+
+        
+    }
+
+    public void phoneUser() {
+        Scanner scanr = new Scanner(System.in);
+        String phoneNo;
+
+        System.out.print("Phone Number: ");
+        phoneNo = scanr.nextLine();
+        this.setEmail(phoneNo);
+        System.out.println("Phone No: " + phoneNo);
+
+        
+    }
+
     public void reservationID(){
-
         Random ranID = new Random();
-
         int radNumbers;
-
        
         // this is a 10 digit unique reservation ID
 
@@ -46,74 +59,39 @@ public class Reservation {
         for(int i=0; i<10; i++){
             //Random Number from 1-10
             radNumbers = ranID.nextInt(10);
-            
+
             //String array for random numbers
             numRan[i] = Integer.toString(radNumbers);
         }
-        
+
         System.out.println("Your Reservation ID is: " + numRan[0] + numRan[1] + numRan[2] + numRan[3] + numRan[4] + numRan[5]
-         + numRan[6] + numRan[7] + numRan[8] + numRan[9]);
+            + numRan[6] + numRan[7] + numRan[8] + numRan[9]);
+
+        
     }
-        
-    /*
-     * Code to allow the user to add their email address
-     */
-        public void emailUser() {
-        	 Scanner scanr = new Scanner(System.in);
-        	
-        	 System.out.print("Email: ");
-             email = scanr.nextLine().toUpperCase();
-             this.setEmail(email);
-             System.out.println("Email: " + email);
-             
-             scanr.close();
-        }
-        
-        /*
-         * Code to allow the user to add their phone number
-         */
-        public void phoneUser() {
-       	 Scanner scanr = new Scanner(System.in);
-       	
-       	 System.out.print("Phone No: ");
-            phoneNo = scanr.nextLine();
-            this.setPhoneNo(phoneNo);
-            System.out.println("Phone No: " + phoneNo);
-            
-            scanr.close();
-       }
 
-        
-        
-    /*
-     * Set and Get methods for the users details
-     */
+    public String getName() {
+        return name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	
-	public String getEmail() {
-		return email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
-	public String getPhoneNo() {
-		return phoneNo;
-	}
+    public String getPhoneNo() {
+        return phoneNo;
+    }
 
-	public void setPhoneNo(String phoneNo) {
-		this.phoneNo = phoneNo;
-	}
-
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
 
 }
