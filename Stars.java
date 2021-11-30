98a6021ce642f3f3f87ecfb904ff15ea09a70f5f
elliotbuckley <91120011+elliotbuckley@users.noreply.github.com>
2021-11-30 16:13:27 +0000

    Add files via upload

--- Stars.java
+++ Stars.java
@@ -1,6 +1,9 @@
+package team18;
+
 import java.util.Scanner;
 
 public class Stars {
+	
     public void selStar(){
         
         int starc;
@@ -15,13 +18,21 @@ public class Stars {
         
         starc = scanr.nextInt();
         
-        if (starc == 1){
+        if (starc == 1)
+        {
             Five five5 = new Five();
             five5.standFive();
             
         }
-        else if(starc == 2){
-           
+        else if(starc == 2)
+        {
+            Four four4 = new Four();
+            four4.standFour();
+        }
+        else
+        {
+            Three three3 = new Three();
+            three3.standThree();
         }
         
         
