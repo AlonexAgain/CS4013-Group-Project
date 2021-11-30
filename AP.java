d89b39aa801ab3a58bbd03a02de88484372b63b5
Saw <76816580+AlonexAgain@users.noreply.github.com>
2021-11-30 17:26:25 +0000

    Add files via upload

--- AP.java
+++ AP.java
@@ -9,7 +9,7 @@ public class AP {
         System.out.println("You have selected our Standard Rates");
         System.out.println("                   ");
         System.out.println("===================");
-        
+
         //Loop throught the menu options from the array
         for(int i = 0; i < options.length-1; i++){
 
@@ -26,23 +26,20 @@ public class AP {
 
     }
 
-    public void apRates(){
+    public void apDD(){
 
         //User input
         int userInput;
 
         // MainMenu Data Array
-        int options[] = {1,2,3,4,5,6,7,8,9,10,11};
+        int options[] = {1,2,3,4,5,6,7};
 
         //String array for choices
-        String optionMenu[] = {"Deluxe Room", "Deluxe Twin", 
-                "Deluxe Single", "Deluxe Family", "Executive Double", 
-                "Executive Twin", "Executive Single", "Classic Double", 
-                "Classic Twin", "Classic Single" ,"Exit"};
+        String optionMenu[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
         //Prices for Rooms
         double redPrices[] = {71.25,71.25,66.5,76,66.5,
-                66.5,61.75,61.75,61.75,47.5};
+                66.5,61.75};
         //Exit the menu
         int Exit = options[options.length-1];
 
@@ -56,52 +53,222 @@ public class AP {
         userInput = scan.nextInt();
 
         //Check users value and looping main menu
-        while(userInput != Exit){ 
-            if (userInput == options[0]){
-                System.out.println(optionMenu[0] + " = " + redPrices[0]);
 
-            }
-            else if (userInput == options[1]){
-
-                System.out.println(optionMenu[1] + " = " + redPrices[1]);
-            }
-            else if (userInput == options[2]){
-                System.out.println(optionMenu[2] + " = " + redPrices[2]);
-            }
-            else if (userInput == options[3]){
-                System.out.println(optionMenu[3] + " = " + redPrices[3]);
-            }
-            else if (userInput == options[4]){
-                System.out.println(optionMenu[4] + " = " + redPrices[4]);
-            }
-            else if (userInput == options[5]){
-                System.out.println(optionMenu[5] + " = " + redPrices[5]);
-            }
-            else if (userInput == options[6]){
-                System.out.println(optionMenu[6] + "=" + redPrices[6]);
-            }
-            else if (userInput == options[7]){
-                System.out.println(optionMenu[7] + " = " + redPrices[7]);
-            }
-            else if (userInput == options[8]){
-                System.out.println(optionMenu[8] + " = " + redPrices[8]);
-            }
-            else if (userInput == options[9]){
-                System.out.println(optionMenu[9] + " = " + redPrices[9]);
-            }
-
-            else if (userInput == options[10]){
-                System.out.println(optionMenu[10] + " = " + redPrices[10]);
-            }
-
-            //Main Menu Again
-            displayMenu2(options, optionMenu, redPrices);
-
-            userInput = scan.nextInt();
-
-            System.out.println(" ");
+        if (userInput == options[0]){
+            System.out.println(optionMenu[0] + " = " + redPrices[0]);
 
         }
-        System.out.println("Thank you for booking with BestSolutions Ltd");
+        else if (userInput == options[1]){
+
+            System.out.println(optionMenu[1] + " = " + redPrices[1]);
+        }
+        else if (userInput == options[2]){
+            System.out.println(optionMenu[2] + " = " + redPrices[2]);
+        }
+        else if (userInput == options[3]){
+            System.out.println(optionMenu[3] + " = " + redPrices[3]);
+        }
+        else if (userInput == options[4]){
+            System.out.println(optionMenu[4] + " = " + redPrices[4]);
+        }
+        else if (userInput == options[5]){
+            System.out.println(optionMenu[5] + " = " + redPrices[5]);
+        }
+        else if (userInput == options[6]){
+            System.out.println(optionMenu[6] + "=" + redPrices[6]);
+        }
+        else if (userInput == options[7]){
+            System.out.println(optionMenu[7] + " = " + redPrices[7]);
+        }
+
+
+        System.out.println(" ");
+        System.out.println("You have Booked: " + userInput);
+
+    }
+
+    public void apDT(){
+
+        //User input
+        int userInput;
+
+        // MainMenu Data Array
+        int options[] = {1,2,3,4,5,6,7};
+
+        //String array for choices
+        String optionMenu[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        //Prices for Rooms
+        double redPrices[] = {71.25,71.25,66.5,76,66.5,
+                66.5,61.75};
+        //Exit the menu
+        int Exit = options[options.length-1];
+
+        //This will display the menu from displayMenu class
+        displayMenu2(options, optionMenu, redPrices);
+
+        //Scanner setup
+        Scanner scan = new Scanner(System.in);
+
+        // Get users input
+        userInput = scan.nextInt();
+
+        //Check users value and looping main menu
+
+        if (userInput == options[0]){
+            System.out.println(optionMenu[0] + " = " + redPrices[0]);
+
+        }
+        else if (userInput == options[1]){
+
+            System.out.println(optionMenu[1] + " = " + redPrices[1]);
+        }
+        else if (userInput == options[2]){
+            System.out.println(optionMenu[2] + " = " + redPrices[2]);
+        }
+        else if (userInput == options[3]){
+            System.out.println(optionMenu[3] + " = " + redPrices[3]);
+        }
+        else if (userInput == options[4]){
+            System.out.println(optionMenu[4] + " = " + redPrices[4]);
+        }
+        else if (userInput == options[5]){
+            System.out.println(optionMenu[5] + " = " + redPrices[5]);
+        }
+        else if (userInput == options[6]){
+            System.out.println(optionMenu[6] + "=" + redPrices[6]);
+        }
+        else if (userInput == options[7]){
+            System.out.println(optionMenu[7] + " = " + redPrices[7]);
+        }
+        
+
+        System.out.println(" ");
+        System.out.println("You have Booked: " + userInput);
+
+    }
+
+    public void apDS(){
+
+        //User input
+        int userInput;
+
+        // MainMenu Data Array
+        int options[] = {1,2,3,4,5,6,7};
+
+        //String array for choices
+        String optionMenu[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        //Prices for Rooms
+        double redPrices[] = {66.50,66.50,66.50,71.25,76,
+                76,61.75};
+        //Exit the menu
+        int Exit = options[options.length-1];
+
+        //This will display the menu from displayMenu class
+        displayMenu2(options, optionMenu, redPrices);
+
+        //Scanner setup
+        Scanner scan = new Scanner(System.in);
+
+        // Get users input
+        userInput = scan.nextInt();
+
+        //Check users value and looping main menu
+
+        if (userInput == options[0]){
+            System.out.println(optionMenu[0] + " = " + redPrices[0]);
+
+        }
+        else if (userInput == options[1]){
+
+            System.out.println(optionMenu[1] + " = " + redPrices[1]);
+        }
+        else if (userInput == options[2]){
+            System.out.println(optionMenu[2] + " = " + redPrices[2]);
+        }
+        else if (userInput == options[3]){
+            System.out.println(optionMenu[3] + " = " + redPrices[3]);
+        }
+        else if (userInput == options[4]){
+            System.out.println(optionMenu[4] + " = " + redPrices[4]);
+        }
+        else if (userInput == options[5]){
+            System.out.println(optionMenu[5] + " = " + redPrices[5]);
+        }
+        else if (userInput == options[6]){
+            System.out.println(optionMenu[6] + "=" + redPrices[6]);
+        }
+        else if (userInput == options[7]){
+            System.out.println(optionMenu[7] + " = " + redPrices[7]);
+        }
+
+
+        System.out.println(" ");
+        System.out.println("You have Booked: " + userInput);
+
+    }
+
+    public void apDF(){
+
+        //User input
+        int userInput;
+
+        // MainMenu Data Array
+        int options[] = {1,2,3,4,5,6,7};
+
+        //String array for choices
+        String optionMenu[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        //Prices for Rooms
+        double redPrices[] = {66.50,66.50,66.50,71.25,76,
+                76,61.75};
+        //Exit the menu
+        int Exit = options[options.length-1];
+
+        //This will display the menu from displayMenu class
+        displayMenu2(options, optionMenu, redPrices);
+
+        //Scanner setup
+        Scanner scan = new Scanner(System.in);
+
+        // Get users input
+        userInput = scan.nextInt();
+
+        //Check users value and looping main menu
+
+        if (userInput == options[0]){
+            System.out.println(optionMenu[0] + " = " + redPrices[0]);
+
+        }
+        else if (userInput == options[1]){
+
+            System.out.println(optionMenu[1] + " = " + redPrices[1]);
+        }
+        else if (userInput == options[2]){
+            System.out.println(optionMenu[2] + " = " + redPrices[2]);
+        }
+        else if (userInput == options[3]){
+            System.out.println(optionMenu[3] + " = " + redPrices[3]);
+        }
+        else if (userInput == options[4]){
+            System.out.println(optionMenu[4] + " = " + redPrices[4]);
+        }
+        else if (userInput == options[5]){
+            System.out.println(optionMenu[5] + " = " + redPrices[5]);
+        }
+        else if (userInput == options[6]){
+            System.out.println(optionMenu[6] + "=" + redPrices[6]);
+        }
+        else if (userInput == options[7]){
+            System.out.println(optionMenu[7] + " = " + redPrices[7]);
+        }
+        
+
+
+        System.out.println(" ");
+        System.out.println("You have Booked: " + userInput);
+
     }
 }
+
